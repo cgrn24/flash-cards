87d9db22067bb5d78c35208c4177922cc4ee9f5b
cgrn24 <stlhnt42@gmail.com>
2023-06-30 03:41:20 +0700

    fix: eslint errors

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -13,6 +13,7 @@ export const Button = <T extends ElementType = 'button'>(
   props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
 ) => {
   const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
+
   return (
     <button
       className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className ?? ''}`}
