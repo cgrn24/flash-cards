25b9a8c1742e71ab446db976f0f0450f0809d297
cgrn24 <stlhnt42@gmail.com>
2023-07-16 22:33:21 +0700

    feat: radio-group added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 
-import { Header } from './components/ui/header/header'
-import { RangeSlider } from './components/ui/slider/slider'
+import { RadioGroup } from './components/ui/radio-group/radio-group'
 
 export function App() {
   const [value, setValue] = useState([25, 75])
 
   return (
     <div>
-      <RangeSlider max={100} min={0} value={value} onChange={setValue} />
+      <RadioGroup values={['aboba', 'not aboba']} />
     </div>
   )
 }
