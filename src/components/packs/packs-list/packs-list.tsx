bacf1b0e8b3f5ab6c7134f35ce677867c0d2689a
cgrn24 <stlhnt42@gmail.com>
2023-09-17 20:03:32 +0700

    feat: packs-list

---
+++ src/components/packs/packs-list/packs-list.tsx
@@ -0,0 +1,15 @@
+import { Typography } from '../../ui'
+import Button from '../../ui/button/button'
+
+import s from './packs-list.module.scss'
+
+export const PacksList = () => {
+  return (
+    <div>
+      <div className={s.header}>
+        <Typography variant="h1">Packs list</Typography>
+        <Button>Add new pack</Button>
+      </div>
+    </div>
+  )
+}
