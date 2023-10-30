358827b5b97bf165f6e4d8ba671a34d2e8d32863
cgrn24 <stlhnt42@gmail.com>
2023-10-30 23:40:09 +0700

    learn-pack: styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { EmptyPack } from './components/packs/empty-pack/empty-pack'
 import { LearnPack } from './components/packs/learn-pack/learn-pack'
 import { Page } from './components/ui/page/page'
 
