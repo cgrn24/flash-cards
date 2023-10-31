50de8f112f36f8fac56b191baac5f87637ba319d
cgrn24 <stlhnt42@gmail.com>
2023-10-31 23:34:17 +0700

    learn-pack: rate pack added

--- src/components/packs/learn-pack/learn-pack.tsx
+++ src/components/packs/learn-pack/learn-pack.tsx
@@ -1,10 +1,16 @@
+import { useState } from 'react'
+
 import { ArrowBack } from '../../../assets/icons'
 import { Card, Typography } from '../../ui'
 import Button from '../../ui/button/button'
+import { RadioGroup } from '../../ui/radio-group/radio-group'
 
 import s from './learn-pack.module.scss'
 
 export const LearnPack = () => {
+  const [showAnswer, setShowAnswer] = useState(false)
+  const rate = ['Did not know', 'Forgor', 'A lot of thought', 'Confused', 'Knew the answer']
+
   return (
     <div className={s.container}>
       <div className={s.back}>
@@ -21,10 +27,28 @@ export const LearnPack = () => {
           <Typography className={s.questionText}>
             <b>Question:</b> How &apos;This&apos; works in JavaScript?
           </Typography>
-          <Typography className={s.darkText}>
+          <Typography variant="body2" className={s.darkText}>
             Количество попыток ответов на вопрос: <b>10</b>
           </Typography>
-          <Button fullWidth>Show answer</Button>
+          {showAnswer && (
+            <>
+              <Typography className={s.answerText}>
+                <b>Answer:</b> This is how &apos;This&apos; works in JavaScript
+              </Typography>
+              <Typography className={s.rateText}>
+                <b>Rate yourself:</b>
+              </Typography>
+              <RadioGroup values={rate} />
+              <Button fullWidth className={s.nextButton}>
+                Next question
+              </Button>
+            </>
+          )}
+          {!showAnswer && (
+            <Button fullWidth onClick={() => setShowAnswer(true)}>
+              Show answer
+            </Button>
+          )}
         </Card>
       </div>
     </div>
