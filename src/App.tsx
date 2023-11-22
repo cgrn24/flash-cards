7029d104907a7b2958b607dfb36eb4aaaf9a2de9
cgrn24 <stlhnt42@gmail.com>
2023-11-22 21:06:08 +0700

    feat: signup route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 
 import { LoginForm } from './components/auth/login-form/login-form'
+import { SignupForm } from './components/auth/signup-form/signup-form'
 import { NotFound } from './components/common/404/not-found'
 import { Page } from './components/ui/page/page'
 
@@ -10,6 +11,7 @@ export function App() {
       <Page>
         <Routes>
           <Route path="/login" element={<LoginForm />} />
+          <Route path="/signup" element={<SignupForm />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Page>
