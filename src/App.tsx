dd141512a96864f5821cb910cdf5e3149a386f49
cgrn24 <stlhnt42@gmail.com>
2023-12-19 22:50:15 +0700

    refactor: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import { Provider } from 'react-redux'
-import { Route, Routes } from 'react-router-dom'
 
-import { LoginForm } from './components/auth/login-form/login-form'
-import { NotFoundPage } from './pages/404/not-found-page'
-import { SignUpPage } from './pages/sign-up-page/sign-up-page'
+import { Router } from './router'
 import { store } from './services/store/store'
 
 export function App() {
   return (
     <Provider store={store}>
-      <Routes>
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Router />
     </Provider>
   )
 }
